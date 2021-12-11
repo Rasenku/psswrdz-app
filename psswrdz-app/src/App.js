1abcdeccb6444c4c38741ffe6c03228561af959b
Dahveyea <dahveyea.cowan@gmail.com>
2021-12-10 16:30:34 -0800

    generate password

--- psswrdz-app/src/App.js
+++ psswrdz-app/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import Password from './password';
+import {useState} from "react"
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducers from './reducers'
+
 
 function App() {
+  const [password, setPassword] = useState()
+  const store = createStore(reducers)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        
+          <Password />
+      </div>
+    </Provider>
   );
 }
 
